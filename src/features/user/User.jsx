fef7321f3fc4e15f914828c618508e5435b6abd2
unknown <somegetiye@gmail.com>
2025-11-21 05:52:52 -0800

    styling the user page

--- src/features/user/User.jsx
+++ src/features/user/User.jsx
@@ -1,4 +1,5 @@
 import { LogOut } from "lucide-react";
+import { Link } from "react-router";
 import styles from "./User.module.css";
 import Button from "../../ui/Button";
 import PageNav from "../../ui/PageNav";
@@ -16,33 +17,61 @@ function User() {
 
 function UserInfo() {
   return (
-    <div className={styles["user-container"]}>
-      <div className={styles["user-header"]} style={{}}>
-        <div>
-          <h2>User Name</h2>
-          <p>User@gmail.com</p>
+    <div className="mx-auto flex max-w-[600px] flex-col items-start justify-start gap-[20px] rounded-lg border-[0.01px] border-stone-700 bg-white px-[25px] py-[30px] shadow-md">
+      {/* <div className={styles["user-container"]}> */}
+      <div className="flex w-full items-center justify-between gap-x-10">
+        {/* <div className={styles["user-header"]} style={{}}> */}
+        <div className="flex flex-col items-center justify-center">
+          <h2 className="text-md font-semibold text-stone-700">User Name</h2>
+          <p className="text-sm text-stone-400">User@gmail.com</p>
         </div>
-
-        <Button type="log-out">
-          <LogOut size="18px" />
-          <p>Logout</p>
-        </Button>
+        <div className="justify-items-end">
+          <Link
+            type="log-out"
+            className="text-md place-items-start bg-transparent px-2 py-1 no-underline"
+          >
+            <p>Logout</p>
+          </Link>
+        </div>
       </div>
-      <hr className={styles.hr}></hr>
+      <div className="flex w-full items-center justify-between">
+        <h4 className="text-md capitalize text-stone-400">Your point</h4>
+        <p className="text-sm text-stone-400">
+          <strong className="text-sm text-stone-500">300.00</strong> points
+        </p>
+      </div>
+      <hr className="border-stone-3000 mt-2 block w-full place-items-center border-t-[0.5px]"></hr>
+      {/* <hr className={styles.hr}></hr> */}
       <div className={styles["acc-detail"]}>
         <h3>Account Details</h3>
         <form action="">
-          <div className={styles["form-el"]}>
-            <label>Name</label>
-            <input type="text" placeholder="Demo User" disabled />
+          <div className="flex flex-col gap-2">
+            {/* <div className={styles["form-el"]}> */}
+            <label className="font-sembold mt-2 text-sm">Name</label>
+            <input
+              type="text"
+              placeholder="Demo User"
+              className="w-[300px] overflow-hidden rounded-md border-[0.1px] py-2 text-center text-stone-500"
+              disabled
+            />
           </div>
-          <div className={styles["form-el"]}>
-            <label>Email</label>
-            <input type="number" placeholder="User@gmail.com" disabled />
+          <div className="flex flex-col gap-2">
+            <label className="font-sembold mt-2 text-sm">Email</label>
+            <input
+              type="number"
+              placeholder="User@gmail.com"
+              className="w-[300px] overflow-hidden rounded-md border-[0.1px] py-2 text-center text-stone-500"
+              disabled
+            />
           </div>
-          <div className={styles["form-el"]}>
-            <label>Member Since</label>
-            <input type="number" placeholder="December 2020" disabled />
+          <div className="flex flex-col gap-2">
+            <label className="font-sembold mt-2 text-sm">Member Since</label>
+            <input
+              type="number"
+              placeholder="December 2020"
+              className="300 w-[300px] overflow-hidden rounded-md border-[0.1px] py-2 text-center text-stone-700 outline-none"
+              disabled
+            />
           </div>
         </form>
       </div>
